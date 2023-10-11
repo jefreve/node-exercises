f74cc9a0c5424981b622e82735b0b054e49f4284
Jef <jefersonrevellobiz@gmail.com>
2023-10-11 11:52:31 +0200

    add json files

---
+++ src/figlet.ts
@@ -0,0 +1,13 @@
+import figlet from "figlet";
+
+figlet(
+  "Artistic banner",
+  function (err: Error | null, data: string | undefined): void {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  }
+);
