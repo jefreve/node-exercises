c457bc8e56deb541201f1374ae2b84b0ea8a241b
Jef <jefersonrevellobiz@gmail.com>
2023-11-11 17:07:56 +0100

    complete exercise

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import 'express-async-errors'
-import multer from 'multer'
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import "express-async-errors";
+import multer from "multer";
 import {
   create,
   deleteById,
@@ -10,60 +10,41 @@ import {
   getOneById,
   updateById,
   createImage,
-} from './controllers/planets'
+} from "./controllers/planets";
 
-import pgPromise from 'pg-promise'
-const db = pgPromise({})('postgres://postgres:postgres@localhost:5432/postgres')
+dotenv.config();
 
-const setupDb = async () => {
-  await db.none(`DROP TABLE IF EXISTS planets;
-
-  CREATE TABLE planets (
-    id SERIAL NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
-    image TEXT
-  );
-  `)
-
-  await db.none(`INSERT INTO planets (name) VALUES ('Earth');`)
-  await db.none(`INSERT INTO planets (name) VALUES ('Mars');`)
-}
-
-setupDb()
-
-export default db
-
-dotenv.config()
-
-const PORT = process.env.PORT || 3000
-const app = express()
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
+    cb(null, file.originalname);
   },
-})
-export const upload = multer({ storage })
+});
+export const upload = multer({ storage });
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/planets', getAll)
+app.get("/api/planets", getAll);
 
-app.get('/api/planets/:id', getOneById)
+app.get("/api/planets/:id", getOneById);
 
-app.post('/api/planets', create)
+app.post("/api/planets", create);
 
-app.put('/api/planets/:id', updateById)
+app.put("/api/planets/:id", updateById);
 
-app.delete('/api/planets/:id', deleteById)
+app.delete("/api/planets/:id", deleteById);
 
-app.post('/api/planets/:id/image', upload.single('image'), createImage)
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
+
+// app.post('/users/login', logIn)
 
 app.listen(PORT, () => {
-  console.log(`App running at http://localhost:${PORT}`)
-})
+  console.log(`App running at http://localhost:${PORT}`);
+});
---
+++ src/passport.ts
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import db from "./db";
+import passport from "passport";
+import passportJWT from "passport-jwt";
+
+const { SECRET } = process.env;
+
+passport.use(
+  new passportJWT.Strategy(
+    {
+      secretOrKey: SECRET,
+      jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (payload, done) => {
+      const user = await db.one(`SELECT * FROM users WHERE id=$1`, payload.id);
+      console.log(user);
+
+      try {
+        return user ? done(null, user) : done(new Error("User not found."));
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
